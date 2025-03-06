4b97e459fb10113c08d447ea62bae2d7f83b99b9
Ismail-n <69399387+Ismail-n@users.noreply.github.com>
2025-03-06 14:31:40 +0000

    added datatypes file and write code of variables in first file

---
+++ node_js_hindi/node-01_veriables.js
@@ -0,0 +1,19 @@
+const accountId = 28948478;
+let accountEmail = "ismaeel.nagori@gmail.com";
+var accountName ="Muhammed Ismaeel";
+accountBranchName = "Mahidpur";
+let accountState ;
+// let's change those values 
+// accountId =33333434; // Not allowed
+
+accountEmail = "in@in.com";
+accountName = "Ubaid";
+accountBranchName ="Ujjain";
+
+/**
+ * Prefer not to use var keyword
+ */
+
+// console.log();
+console.table([accountEmail, accountName , accountBranchName ,accountState ]);
+
---
+++ node_js_hindi/node-02_datatypes.js
@@ -0,0 +1,7 @@
+let stringVariableName = "Ubaidullah";
+
+let integerVariableMobileNumber = 9039687032;
+
+let booleanVariableIsAdmittedInSchool = false;
+
+let arrayVariableHobbies = ["playing" , "Walking" , "Praying"];
